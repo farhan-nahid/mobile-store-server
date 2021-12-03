7203953b360e9f4793acc9fb58fc9acafe38d6c7
gesora96 <nahidahmed9933@gmail.com>
2021-12-04 05:29:40 +0600

    almost done

--- index.js
+++ index.js
@@ -31,6 +31,64 @@ async function run() {
     const database = client.db(`${process.env.DB_NAME}`);
     const mobileCollection = database.collection('all_mobile');
     const reviewsCollection = database.collection('reviews');
+    const orderCollection = database.collection('orders');
+
+    // GET all products data
+
+    app.get('/all-products', async (req, res) => {
+      const cursor = mobileCollection.find({});
+      const products = await cursor.toArray();
+      res.json(products);
+    });
+
+    // GET a single place by ID
+
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const mobile = await mobileCollection.findOne(query);
+      res.send(mobile);
+    });
+
+    // GET all reviews data
+
+    app.get('/all-reviews', async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const allReviews = await cursor.toArray();
+      res.json(allReviews);
+    });
+
+    // GET all orders data
+
+    app.get('/all-orders', async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      console.log(email);
+      if (email) {
+        query = { email };
+      }
+      const cursor = orderCollection.find(query);
+      const allOrders = await cursor.toArray();
+      res.json(allOrders);
+    });
+
+    // POST a single product
+
+    app.post('/add-product', async (req, res) => {
+      const mobile = req.body;
+      const result = await mobileCollection.insertOne(mobile);
+      res.json(result);
+    });
+
+    // POST a order
+
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+
+    // POST a single review
 
     app.post('/add-reviews', async (req, res) => {
       const review = req.body;
@@ -38,10 +96,37 @@ async function run() {
       res.json(result);
     });
 
-    app.get('/all-reviews', async (req, res) => {
-      const cursor = reviewsCollection.find({});
-      const allReviews = await cursor.toArray();
-      res.json(allReviews);
+    // PUT order status
+
+    app.put('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const filter = { _id: ObjectId(id) };
+      const updatingStatus = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = orderCollection.updateOne(filter, updatingStatus);
+      res.json(result);
+    });
+
+    // DELETE a single product by ID
+
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await mobileCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // DELETE a single order by ID
+
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
     });
   } finally {
     // client.close()
